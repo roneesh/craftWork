abbf88fd760bcd42f4faebef492b6e1db34968a3
Roneesh Vashisht <rvashis@searshc.com>
2014-10-18 20:35:42 -0500

    gallery stable

--- js/craftWork.js
+++ js/craftWork.js
@@ -6,18 +6,11 @@ $(document).ready(function() {
 	// 	}
 	// })
 
-	var el = $('.text-box'), //jQuery element
-		del = $('.text-box')[0], //DOM element
-		lineHeight = $('.text-box p').css('lineHeight');
+	var el = $('.text-description'), //jQuery element
+		del = $('.text-description')[0], //DOM element
+		lineHeight = $('.text-description p').css('lineHeight');
 
 	el.scroll(function() {
-		if (del.scrollTop > 20) {
-			$('.top-white').css('visibility', 'visible');
-		}
-		else if (del.scrollTop < 100) {
-			$('.top-white').css('visibility', 'hidden');
-		}
-
 		if (del.scrollHeight - el.scrollTop() <= (el.outerHeight() + 22) ) {
 			$('.bottom-white').css('visibility', 'hidden');
 		}
@@ -30,50 +23,35 @@ $(document).ready(function() {
 		firstDescriptionBlock = firstWork.children('.ribbon-description'),
 		firstZoomImageURL = firstDescriptionBlock.data('zoom');
 		firstImageURL = firstDescriptionBlock.data('image'),
-		workArtist = firstDescriptionBlock.data('artist'),
-		workTitle = firstDescriptionBlock.data('title'),
-		workMedium = firstDescriptionBlock.data('medium'),
-		workDate = firstDescriptionBlock.data('date');
+		descriptionText = firstDescriptionBlock.html(),
+		$easyzoom = $('.easyzoom').easyZoom(),
+		api = $easyzoom.data('easyZoom');;
 
 		firstWork.addClass('selected');
 		$('.work-image img').attr('src', firstImageURL);
 		$('.work-image a').attr('href', firstZoomImageURL);
-		$('.artist-name').html(workArtist)
-		$('.work-title').html(workTitle)
-		$('.work-medium').html(workMedium)
-		$('.work-date').html(workDate);
-
-	var $easyzoom = $('.easyzoom').easyZoom();
-	var api = $easyzoom.data('easyZoom');
+		$('.work-description').html(descriptionText);
 
 	$('.ribbon-work').on('click', function() {
 		var descriptionBlock = $(this).children('.ribbon-description'),
 			zoomImageURL = descriptionBlock.data('zoom');
 			largeImageURL = descriptionBlock.data('image'),
-			workArtist = descriptionBlock.data('artist'),
-			workTitle = descriptionBlock.data('title'),
-			workMedium = descriptionBlock.data('medium'),
-			workDate = descriptionBlock.data('date'),
 			descriptionText = descriptionBlock.html();
 			
-			// active image
-			$('.ribbon-work').removeClass('selected');
+			// set active image
 			$('.work-image img').attr('src', largeImageURL);
 			$('.work-image a').attr('href', zoomImageURL);
-			$('.artist-name').html(workArtist)
-			$('.work-title').html(workTitle)
-			$('.work-medium').html(workMedium)
-			$('.work-date').html(workDate);
 			$('.work-description').html(descriptionText);
 
+			// set the active image border
+			$('.ribbon-work').removeClass('selected');
 			$(this).addClass('selected');
+
+			// reset the image zoom
 			api.teardown();
 			$easyzoom = $('.easyzoom').easyZoom();
 			api = $easyzoom.data('easyZoom');
 	});
 
-	// EasyZoom
-
-	
 
 });
