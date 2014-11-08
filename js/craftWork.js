8ba6ed01567a16718a077669b3f4eb6eb80d76d5
Roneesh <roneesh@gmail.com>
2014-11-08 13:06:07 -0600

    adding wonder theme to project

--- js/craftWork.js
+++ js/craftWork.js
@@ -6,12 +6,7 @@ $(document).ready(function() {
 	// 	}
 	// })
 	// $.fn.ekkoLightbox.defaults({always_show_close: false});
-	$(document).delegate('.zoomContainer', 'click', function(event) {
-	    event.preventDefault();
-	    $(this).children('a').ekkoLightbox({always_show_close: false});
-	 //    $easyzoomlightbox = $('.ekko-lightbox-container div').easyZoom(),
-		// apilightbox = $easyzoomlightbox.data('easyZoom');;
-	});
+	
 
 	var el = $('.text-description'), //jQuery element
 		del = $('.text-description')[0], //DOM element
@@ -24,42 +19,6 @@ $(document).ready(function() {
 		else {
 			$('.bottom-white').css('visibility', 'visible');
 		}
-	});
-
-	var firstWork = $('.ribbon-work').first(),
-		firstDescriptionBlock = firstWork.children('.ribbon-description'),
-		firstZoomImageURL = firstDescriptionBlock.data('zoom');
-		firstImageURL = firstDescriptionBlock.data('image'),
-		descriptionText = firstDescriptionBlock.html(),
-		$easyzoom = $('.easyzoom').easyZoom(),
-		api = $easyzoom.data('easyZoom');;
-
-		firstWork.addClass('selected');
-		$('.work-image img').attr('src', firstImageURL);
-		$('.work-image a').attr('href', firstZoomImageURL);
-		$('.work-description').html(descriptionText);
-
-	$('.ribbon-work').on('click', function() {
-		var descriptionBlock = $(this).children('.ribbon-description'),
-			zoomImageURL = descriptionBlock.data('zoom');
-			largeImageURL = descriptionBlock.data('image'),
-			descriptionText = descriptionBlock.html();
-			
-			$('work-description').html('');
-
-			// set active image
-			$('.work-image img').attr('src', largeImageURL);
-			$('.work-image a').attr('href', zoomImageURL);
-			$('.work-description').html(descriptionText);
-
-			// set the active image border
-			$('.ribbon-work').removeClass('selected');
-			$(this).addClass('selected');
-
-			// reset the image zoom
-			api.teardown();
-			$easyzoom = $('.easyzoom').easyZoom();
-			api = $easyzoom.data('easyZoom');
 	});
 
 
---
+++ js/gallery.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+	$(this).delegate('.zoomContainer', 'click', function(event) {
+	    event.preventDefault();
+	    $(this).children('a').ekkoLightbox({always_show_close: false});
+	 	//  $easyzoomlightbox = $('.ekko-lightbox-container div').easyZoom(),
+		// apilightbox = $easyzoomlightbox.data('easyZoom');;
+	});
+
+	var firstWork = $('.ribbon-work').first(),
+	firstDescriptionBlock = firstWork.children('.ribbon-description'),
+	firstZoomImageURL = firstDescriptionBlock.data('zoom');
+	firstImageURL = firstDescriptionBlock.data('image'),
+	descriptionText = firstDescriptionBlock.html(),
+	$easyzoom = $('.easyzoom').easyZoom(),
+	api = $easyzoom.data('easyZoom');;
+
+	firstWork.addClass('selected');
+	$('.work-image img').attr('src', firstImageURL);
+	$('.work-image a').attr('href', firstZoomImageURL);
+	$('.work-description').html(descriptionText);
+
+	$('.ribbon-work').on('click', function() {
+		var descriptionBlock = $(this).children('.ribbon-description'),
+			zoomImageURL = descriptionBlock.data('zoom');
+			largeImageURL = descriptionBlock.data('image'),
+			descriptionText = descriptionBlock.html();
+			
+			$('work-description').html('');
+
+			// set active image
+			$('.work-image img').attr('src', largeImageURL);
+			$('.work-image a').attr('href', zoomImageURL);
+			$('.work-description').html(descriptionText);
+
+			// set the active image border
+			$('.ribbon-work').removeClass('selected');
+			$(this).addClass('selected');
+
+			// reset the image zoom
+			api.teardown();
+			$easyzoom = $('.easyzoom').easyZoom();
+			api = $easyzoom.data('easyZoom');
+	});
+
+});
